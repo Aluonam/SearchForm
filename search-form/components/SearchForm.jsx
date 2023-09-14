5fccabe3d73f47174e0370b07c220a66e2d9e7f0
Paula Cano <paulacanoroman@gmail.com>
2023-09-14 21:07:22 +0200

    funcionalidad básica consumo api y manejo botón

--- search-form/components/SearchForm.jsx
+++ search-form/components/SearchForm.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect, lazy } from 'react'
 
 const SearchForm = () => {
 
+    const [dataAPI, setDataAPI] = useState([])
+    const [userData, setUserData] = useState([])
+
+    useEffect(() => {
+      llamadaAPI()
+    }, [])
+    
+    const llamadaAPI = async ()=>{
+        try{
+            const llamada = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+            const datos = await llamada.json();
+            console.log(datos)
+            setDataAPI(datos)
+        } catch(error){"error detected", error}
+        }
+    
+
+    const handleSendData = ()=>{
+        console.log(userData)
+    }
 
 
   return (
     <>
-    <input></input>
-    <button></button>
+    Escribe el id que quieres buscar
+    <input type='text' onChange={(e)=>{setUserData(e.target.value)}}></input>
+    <button onClick={()=>{handleSendData()}}>Enviar</button>
     </>
   )
 }
